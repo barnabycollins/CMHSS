5b8966e36bc0a46e2457eff8da5700cf354f36f9
Barnaby Collins <barnabyc@sky.com>
2021-05-22 00:50:12 +0100

    Refactor into separate utils file, add mixed use analysis

--- accessibility.py
+++ accessibility.py
@@ -1,115 +1,58 @@
-import pydriosm.reader
-import time
-import json
-from shapely.geometry import shape
-import re
-import pickle
-import pandas
+from utils import *
+from tqdm import tqdm
+import math
 
-def parsePBF(cityFile: str, useCache: bool = True):
-    cityName = re.search(r'^(?:.*\/)?([a-zA-Z]+)\.osm\.pbf$', cityFile).group(1)
-    cacheName = f"./cache/{cityName}.pickle"
+def generateScore(city: dict, doTransport = True, doMixedUse = True):
+    """Generates and returns an accessibility score for a given parsed city file"""
+    print("Analysing city...\n")    
 
-    if useCache:
-        try:
-            with open(cacheName, 'rb') as file:
-                print(f"Loading data from '{cacheName}'...")
-                city = pickle.load(file)
-            
-            print(f"Data loaded.")
-
-        except IOError:
-            useCache = False
-            print(f"No cache found for '{cityName}'. Reading in city file...")
-
-    if (not useCache):
-        start = time.time()
-        city = pydriosm.reader.parse_osm_pbf(cityFile, transform_other_tags=True)
-        end = time.time()
-
-        print(f"Loaded {cityFile} in {(end-start):.2f} seconds. Writing to cache...")
-
-        with open(cacheName, 'wb') as file:
-            pickle.dump(city, file)
-
-    return city
-
-def tagSearch(tags, item):
-    
-    if (isinstance(tags, pandas.Series)):
-        tags = tags.to_dict()
-    
-    elif (tags == None):
-        return None
-
-    if (item not in tags):
-        if ("other_tags" in tags):
-            return tagSearch(tags["other_tags"], item)
+    if (doTransport):
+        print(f"\n\n===== METRO / TRAIN NETWORK REPORT =====")
         
-        return None
-    
-    return tags[item]
+        stationLineCounts = []
+        totalStations = 0
+        numWithNoLines = 0
 
-def findTags(struct, tags):
-    out = []
-    for i in tags:
-        out.append(tagSearch(struct, i))
-    
-    return out
+        for i in tqdm(range(len(city["points"])), desc="Analysing transport network"):
+            struct = city["points"].loc[i]
 
-def generateScore(city: dict):
-    print("Analysing city...\n")
-    
+            [name, railway, usage, line] = findTags(struct, ["name", "railway", "usage", "line"])
 
-    #print("\n\n===== POINTS =====\n")
+            if (railway == "station" and name != None and usage != "tourism"):
+                totalStations += 1
+                num_lines = 1
 
+                if (line != None):
+                    num_lines = line.count(";") + 1
+                
+                else:
+                    numWithNoLines += 1
+
+                #stations[name] = num_lines
+                stationLineCounts.append(num_lines)
         
-    stations = {}
-    stationLineCounts = []
-    totalStations = 0
-    numWithNoLines = 0
+        
+        # based on https://blog.finxter.com/how-to-get-the-standard-deviation-of-a-python-list/
+        averageNumLines = sum(stationLineCounts) / len(stationLineCounts)
+        variance = sum([(n - averageNumLines)**2 for n in stationLineCounts]) / len(stationLineCounts)
+        std = variance**0.5
 
-    for i in range(len(city["points"])):
-        struct = city["points"].loc[i]
+        percentWithNoLines = numWithNoLines*100/totalStations
+        qualityStatement = ""
+        skipSection = False
+        if (percentWithNoLines == 0):
+            qualityStatement = f"\nAll stations had line information. This value is very reliable."
+        elif(percentWithNoLines > 90):
+            print(f"\nAn extremely small proportion ({100-percentWithNoLines:.1f}%) of stations had line data. For this reason, data is not reliable.")
+            skipSection = True
+        elif (percentWithNoLines > 50):
+            qualityStatement = "\nThis is over 50%: treat the returned standard deviation value with caution as there are a significant number of lines that have gone unaccounted for!"
+        elif (percentWithNoLines < 20):
+            qualityStatement = "\nThis is less than 20%, so this value is reasonably reliable."
 
-        [name, railway, usage, line] = findTags(struct, ["name", "railway", "usage", "line"])
-
-        if (railway == "station" and name != None and usage != "tourism"):
-            totalStations += 1
-            num_lines = 1
-
-            if (line != None):
-                print(line)
-                num_lines = line.count(";") + 1
-            
-            else:
-                numWithNoLines += 1
-
-            #stations[name] = num_lines
-            stationLineCounts.append(num_lines)
-    
-
-
-    print(f"\n\n===== METRO / TRAIN NETWORK REPORT =====")
-    # based on https://blog.finxter.com/how-to-get-the-standard-deviation-of-a-python-list/
-    averageNumLines = sum(stationLineCounts) / len(stationLineCounts)
-    variance = sum([(n - averageNumLines)**2 for n in stationLineCounts]) / len(stationLineCounts)
-    std = variance**0.5
-
-    percentWithNoLines = numWithNoLines*100/totalStations
-    qualityStatement = ""
-    skipSection = False
-    if (percentWithNoLines > 90):
-        print(f"\nAn extremely small proportion ({100-percentWithNoLines:.1f}%) of stations had line data. For this reason, data is not reliable.")
-        skipSection = True
-    elif (percentWithNoLines > 50):
-        qualityStatement = "\nThis is over 50%: treat the returned standard deviation value with caution as there are a significant number of lines that have gone unaccounted for!"
-    elif (percentWithNoLines < 20):
-        qualityStatement = "\nThis is less than 80%, so this value is reasonably reliable."
-
-    if (not skipSection):
-        print(f"Standard deviation in number of lines per station is {std}.")
-        print(f"{percentWithNoLines:.1f}% of stations had no line information.{qualityStatement}")
+        if (not skipSection):
+            print(f"Standard deviation in number of lines per station is {std}.")
+            print(f"{percentWithNoLines:.1f}% of stations had no line information.{qualityStatement}")
 
     """
     print("\n\n===== LINES =====\n")
@@ -140,27 +83,56 @@ def generateScore(city: dict):
     print("\n\n===== MULTIPOLYGONS =====\n")
     """
 
-    dwellings = {
-        "houses": [],
-        "flats": []
-    }
+    if (doMixedUse):
+        dwellings = []
 
-    schools = []
+        schools = []
 
-    for i in range(len(city["multipolygons"])):
-        struct = city["multipolygons"].loc[i]
+        for i in tqdm(range(len(city["multipolygons"])), desc="Getting building data"):
+            struct = city["multipolygons"].loc[i]
+            coordinate = getCoord(struct["coordinates"])
 
-        [building, buildingFlats, amenity] = findTags(struct, ["building", "building:flats", "amenity"])
-
-        if (building == "house"):
-            dwellings["houses"].append(struct)
+            [building, buildingFlats, amenity] = findTags(struct, ["building", "building:flats", "amenity"])
+            
+            if (buildingFlats != None):
+                dwellings.append((buildingFlats, coordinate))
+            
+            elif (building == "house"):
+                dwellings.append((1, coordinate))
+            
+            elif (building == "apartments"):
+                dwellings.append((30, coordinate))
+            
+            elif (building == "residential"):
+                dwellings.append((3, coordinate))
+            
+            elif (amenity == "school"):
+                schools.append(coordinate)
         
-        elif (buildingFlats != None):
-            dwellings["flats"].append(struct)
+        schools = sorted(schools, key=lambda x: x[0])
+
+        totalHouseholds = 0
+        totalDistance = 0
+        for h in tqdm(dwellings, desc="Analysing zoning"):
+            coords = h[1]
+            numHouseholds = h[0]
+
+            nearest = findMinDistance(coords, schools)
+
+            if (type(numHouseholds) != int):
+                try:
+                    numHouseholds = int(numHouseholds)
+                
+                except:
+                    numHouseholds = 5
+                
+            totalHouseholds += numHouseholds
+            totalDistance += numHouseholds * nearest
+
+        avgDistanceToSchool = totalDistance / totalHouseholds
         
-        elif (amenity == "school"):
-            print(struct, "geometry")
-            schools.append(struct)
+        print(f"Average distance to school: {avgDistanceToSchool*1000:.0f}m")
+
     
     """
     print("\n\n===== OTHER RELATIONS =====\n")
@@ -181,6 +153,7 @@ if (__name__ == "__main__"):
     """
     parsePBF("data/Southfields.osm.pbf")
     parsePBF("data/Durham.osm.pbf")
+    parsePBF("data/Newcastle.osm.pbf")
     parsePBF("data/Seoul.osm.pbf")
     parsePBF("data/Vienna.osm.pbf")
     parsePBF("data/Edinburgh.osm.pbf")
@@ -190,5 +163,5 @@ if (__name__ == "__main__"):
     """
 
     #"""
-    generateScore(parsePBF("data/Newcastle.osm.pbf"))
+    generateScore(parsePBF("data/NewYork.osm.pbf"), doTransport=False, doMixedUse=True)
     #"""
---
+++ utils.py
@@ -0,0 +1,187 @@
+import pydriosm.reader
+import time
+import re
+import pickle
+import pandas
+import math
+
+def parsePBF(cityFile: str, useCache: bool = True, writeCache: bool = True):
+    """Parses a named .osm.pbf file into a Python structure and returns it.
+        Writes loaded files to a cache directory to save time in future, and
+        reads any matching cache file back if it exists (both by default)
+        - cityFile: should be relative to the working directory or otherwise absolute
+        - useCache: set false to force reading from the PBF instead of cache
+        - writeCache: set false to stop the function writing to cache
+    """
+
+    cityName = re.search(r'^(?:.*\/)?([a-zA-Z]+)\.osm\.pbf$', cityFile).group(1)
+    cacheName = f"./cache/{cityName}.pickle"
+
+    if useCache:
+        try:
+            with open(cacheName, 'rb') as file:
+                print(f"Loading data from '{cacheName}'...")
+                city = pickle.load(file)
+            
+            print(f"Data loaded.")
+
+        except IOError:
+            useCache = False
+            print(f"No cache found for '{cityName}'. Reading in city file...")
+
+    if (not useCache):
+        start = time.time()
+        city = pydriosm.reader.parse_osm_pbf(cityFile, transform_other_tags=True)
+        end = time.time()
+
+        print(f"Loaded {cityFile} in {(end-start):.2f} seconds. Writing to cache...")
+
+        if (writeCache):
+            with open(cacheName, 'wb') as file:
+                pickle.dump(city, file)
+            print(f"Wrote pickle file to '{cacheName}''.")
+
+    return city
+
+
+def tagSearch(tags, item):
+    """Searches for a tag with the name 'item' in the given structure.
+        Returns the value of that tag, or otherwise None if not present.
+        - tags: the structure to search
+        - item: the tag name to search for
+    """
+    
+    if (isinstance(tags, pandas.Series)):
+        tags = tags.to_dict()
+    
+    elif (tags == None):
+        return None
+
+    if (item not in tags):
+        if ("other_tags" in tags):
+            return tagSearch(tags["other_tags"], item)
+        
+        return None
+    
+    return tags[item]
+
+
+def findTags(struct, tags):
+    """Bulk searches for tags in a structure, and returns them in the same order as given.
+        - struct: the structure to be searched
+        - tags: an ordered list of tag names to be returned in a list with the same order
+    """
+    out = []
+    for i in tags:
+        out.append(tagSearch(struct, i))
+    
+    return out
+
+
+def getCoord(coordinates):
+    """Returns a single co-ordinate pair from the given arbitrary set of co-ordinates
+        Recursively traverses down the layers of a nested list until a pair of floats is located.
+        Useful as a metric for roughly where an object is.
+        - coordinates: an arbitrarily structured nested list of co-ordinates
+    """
+    if (type(coordinates[0]) != float):
+        return getCoord(coordinates[0])
+    
+    else:
+        if (len(coordinates) == 2):
+            return coordinates
+        
+        else:
+            return None
+
+
+def findDistance(coord1, coord2):
+    """Calculates the rough surface distance between two lat/long co-ordinate pairs
+        Adapted from https://stackoverflow.com/a/19412565
+        - coord1: the first co-ordinate
+        - coord2: the second co-ordinate
+    """
+
+    from math import sin, cos, sqrt, atan2, radians
+
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(coord1[1])
+    lon1 = radians(coord1[0])
+    lat2 = radians(coord2[1])
+    lon2 = radians(coord2[0])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
+
+
+def findMinDistance_Naive(centre, locations):
+    """Exhaustively enumerates locations to find the nearest one to the centre point,
+        and returns the distance between these two points.
+        - centre: the reference point to search for nearest neighbours to
+        - locations: the list of co-ordinates to search for the neighbours/
+    """
+    distances = []
+
+    for s in locations:
+        distances.append(findDistance(centre, s))
+    
+    return min(distances)
+
+
+def findLongitudeRange(centre, distance):
+    """Given a distance and a centre point, find the two longitudes
+        at that distance east and west of that point.
+        - centre: the point to compute the range either side of
+        - distance: the distance in kilometres to cover either side of the centre point
+    """
+    # approximate circumference of earth in km
+    C = 40042.0
+
+    dL = (distance / C) * 360 + 0.05
+
+    return (centre - dL, centre + dL)
+
+
+def findMinDistance(centre, locations):
+    """Finds the distance from 'centre' to the nearest co-ordinate in 'locations'.
+        First samples a few locations, then takes the distance to the nearest and
+        ignores any co-ordinates that are further in longitude than that distance.
+        - centre: a pair of co-ordinates in the format [longitude, latitude]
+        - locations: a list of co-ordinates in the above format, SORTED BY LONGITUDE!
+    """
+    
+    distances = []
+
+    n_locations = len(locations)
+    n_samples = min(n_locations, 100)
+
+    for s in range(n_samples):
+        sampleLoc = locations[math.floor(s * (n_locations / n_samples))]
+        distances.append(findDistance(centre, sampleLoc))
+
+    if (n_locations > n_samples):
+
+        nearest = min(distances)
+        rangeToSearch = findLongitudeRange(centre[0], nearest)
+        distances = [nearest]
+
+        for l in locations:
+            if (l[0] < rangeToSearch[0]):
+                continue
+
+            elif (l[0] > rangeToSearch[1]):
+                break
+
+            distances.append(findDistance(centre, l))
+
+    nearest = min(distances)
+
+    return nearest
+        
